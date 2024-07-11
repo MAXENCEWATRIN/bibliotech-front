e594e94930e89d048351e356bf4108bd675c66ce
Maxence <maxence.watr@gmail.com>
2024-07-11 18:23:38 +0200

    Carousel En place

---
+++ src/components/BookCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="book-card">
+        <img :src="book.coverPageUrl" alt="Book Cover" class="book-cover" />
+        <div class="book-details">
+            <h2>{{ book.title }}</h2>
+            <p>{{ book.authorName || 'Unknown Author' }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { BookResponse } from '../model/BookResponse';
+
+export default defineComponent({
+    props: {
+        book: {
+            type: Object as PropType<BookResponse>,
+            required: true
+        }
+    }
+});
+</script>
+
+<style>
+.book-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.book-cover {
+    max-width: 100%;
+    height: auto;
+    border-radius: 10px;
+}
+
+.book-details {
+    margin-top: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,24 @@
       <li v-for="book in response.data" :key="book.id">
         <router-link :to="{ name: 'BookForm', params: { id: book.id } }">{{ book.title }}</router-link>
         <button @click="deleteBook(book.id)">Delete</button>
+
       </li>
     </ul>
+  </div>
+  <div>
+    <h1>Books Carousel</h1>
+    <Carousel :items-to-show="2.5" :wrap-around="true">
+      <Slide v-for="book in response.data" :key="book.id">
+        <div class="carousel__item">
+          <BookCard :book="book" />
+        </div>
+
+      </Slide>
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
   </div>
 </template>
 
@@ -15,17 +31,27 @@
 import { defineComponent } from 'vue';
 import axios from 'axios';
 import type { GetAllBooksResponse } from '../model/GetAllBooksResponse';
-
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import BookCard from '../components/BookCard.vue';
 
 export default defineComponent({
+  name: 'WrapAround',
+  components: {
+    Carousel,
+    Slide,
+    BookCard,
+    Pagination,
+    Navigation,
+  },
   data() {
-      return {
-        response: {
+    return {
+      response: {
         success: false,
         data: {}
       } as GetAllBooksResponse
     };
-    },
+  },
   methods: {
     fetchBooks() {
       axios.get('http://localhost:8080/biblioto/books')
@@ -51,3 +77,26 @@ export default defineComponent({
   }
 });
 </script>
+<style>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
